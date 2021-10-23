8c1ea80e973f5a52bdc1d95923cfcbaf7206b96a
Ageler <andrei-borchenko@mail.ru>
2021-10-23 17:38:18 +0300

    Техническое задание. Агрегатор Java Вакансий. [#260357]

---
+++ src/main/java/Job4j/it/TestClass.java
@@ -0,0 +1,7 @@
+package Job4j.it;
+
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/test/java/Job4j/it/TestClassTest.java
@@ -0,0 +1,13 @@
+package Job4j.it;
+
+
+import org.junit.Test;
+
+
+public class TestClassTest {
+
+    @Test
+    public void testing() {
+    }
+
+}
