4b80cd52150263c0cdfef91c5c29f60b81a6b7c7
Ageler <andrei-borchenko@mail.ru>
2021-10-27 14:39:54 +0300

    Парсинг HTML страницы. [#260358]

--- src/main/java/ru/job4j/quartz/SqlRuParse.java
+++ src/main/java/ru/job4j/quartz/SqlRuParse.java
@@ -8,9 +8,13 @@ import org.jsoup.select.Elements;
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            System.out.println(td.children());
+        Elements row = doc.select("tr");
+        for (int i = 4; i < row.size(); i++) {
+            Element element = row.get(i);
+            if (element.childrenSize() > 2) {
+                System.out.println(element.child(5).text());
+            }
+
         }
     }
 }
