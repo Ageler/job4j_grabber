a3828ade347285f37627af07654c9b5a3cc7cec1
Ageler <andrei-borchenko@mail.ru>
2021-10-29 03:46:00 +0300

    Модель данных - Post

--- src/main/java/ru/job4j/quartz/Post.java
+++ src/main/java/ru/job4j/quartz/Post.java
@@ -2,14 +2,35 @@ package ru.job4j.quartz;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode
 @Getter
 @Setter
 @ToString
 public class Post {
     private int id;
-    private String name;
+    private String title;
+    private String link;
     private String description;
+    private LocalDateTime created;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id == post.id && link.equals(post.link) && Objects.equals(description, post.description) && created.equals(post.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, link, description, created);
+    }
 }
